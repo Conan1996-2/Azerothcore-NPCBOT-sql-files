ed0b724daf89fead1124980d67b7ccc40dde1333
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-05-28 11:28:45 -0400

    Add SQL script to remove 11k bots from database

---
+++ 11kBots/remove_11k_bots.sql
@@ -0,0 +1,5 @@
+DELETE FROM `creature_equip_template` WHERE `CreatureID` BETWEEN 80001 AND 91982;
+DELETE FROM `creature_template` WHERE `entry` BETWEEN 80001 AND 91982;
+DELETE FROM `creature_template_model` WHERE `CreatureID` BETWEEN 80001 AND 91982;
+DELETE FROM `creature_template_npcbot_appearance` WHERE `entry` BETWEEN 80001 AND 91982;
+DELETE FROM `creature_template_npcbot_extras` WHERE `entry` BETWEEN 80001 AND 91982;
